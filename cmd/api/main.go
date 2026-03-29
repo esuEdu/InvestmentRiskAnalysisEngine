6693cf167573d88c888e38912d81956125535fd3
C1353103 Eduardo Regis Vieira <c1353103@interno.bb.com.br>
2026-03-29 02:03:31 -0300

    Add postgres connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,28 @@
 package main
 
-import "github.com/esuEdu/investment-risk-engine/internal/server"
+import (
+	"log"
+
+	"github.com/esuEdu/investment-risk-engine/internal/config"
+	"github.com/esuEdu/investment-risk-engine/internal/db"
+	"github.com/esuEdu/investment-risk-engine/internal/server"
+)
 
 func main() {
+
+	cfg := config.Load()
+
+	_, err := db.NewPostgres(
+		cfg.DBHost,
+		cfg.DBPort,
+		cfg.DBUser,
+		cfg.DBPassword,
+		cfg.DBName,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	s := server.New()
 	s.Start(":8080")
 }
