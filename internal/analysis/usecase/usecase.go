b5ba1892dbecaba583969f9297d1190004062333
C1353103 Eduardo Regis Vieira <c1353103@interno.bb.com.br>
2026-04-11 11:13:44 -0300

    Add new usecases

--- internal/analysis/usecase/usecase.go
+++ internal/analysis/usecase/usecase.go
@@ -32,3 +32,29 @@ func (u *UseCase) ExecuteCreate(ctx context.Context, portfolioID uuid.UUID, benc
 
 	return created, nil
 }
+
+func (u *UseCase) ExecuteGet(ctx context.Context, AnalysisID uuid.UUID) (domain.AnalysisRequest, error) {
+	analysis, err := u.repo.Get(ctx, AnalysisID)
+	if err != nil {
+		return domain.AnalysisRequest{}, err
+	}
+
+	return analysis, err
+}
+
+func (u *UseCase) ExecuteUpdate(ctx context.Context, AnalysisID uuid.UUID, status domain.Status) error {
+	if err := u.repo.UpdateStatus(ctx, AnalysisID, status); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (u *UseCase) ExecuteList(ctx context.Context, limit, offset int32, status *domain.Status) ([]domain.AnalysisRequest, error) {
+	listed, err := u.repo.List(ctx, limit, offset, (*string)(status))
+	if err != nil {
+		return nil, err
+	}
+
+	return listed, nil
+}
