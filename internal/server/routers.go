5367b056def0bc65a89fb2f5871caec9dfb1374d
C1353103 Eduardo Regis Vieira <c1353103@interno.bb.com.br>
2026-04-21 09:33:19 -0300

    Add new routes

--- internal/server/routers.go
+++ internal/server/routers.go
@@ -4,13 +4,16 @@ import "github.com/gin-gonic/gin"
 
 func (s *Server) setupRouter() {
 
-	api := s.router.Group("/api")
+	api := s.router.Group("/api/v1")
 	{
 		api.GET("/health", s.healthCheck)
 		// Analysis Routes
 		analysis := api.Group("/analyses")
 		{
 			analysis.POST("", s.analysisHandler.Create)
+			analysis.GET("", s.analysisHandler.Get)
+			analysis.GET("", s.analysisHandler.List)
+			analysis.PUT("", s.analysisHandler.Update)
 		}
 	}
 }
